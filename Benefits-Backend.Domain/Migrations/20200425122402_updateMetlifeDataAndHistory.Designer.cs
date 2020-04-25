751de6072c59da37603e3e3b965efd82e41f90cf
Hagar <hagar.aboelfottouh1@vodafone.com>
2020-04-25 14:27:22 +0200

    migrations

--- Benefits-Backend.Domain/Migrations/20200425122402_updateMetlifeDataAndHistory.Designer.cs
+++ Benefits-Backend.Domain/Migrations/20200425122402_updateMetlifeDataAndHistory.Designer.cs
@@ -4,14 +4,16 @@ using Benefits_Backend.Domain.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Benefits_Backend.Domain.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20200425122402_updateMetlifeDataAndHistory")]
+    partial class updateMetlifeDataAndHistory
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
