4f7889c682336e88aaad08290c518f4dfe2e77e1
Hagar <hagar.aboelfottouh1@vodafone.com>
2020-04-27 16:11:17 +0200

    add the repository

---
+++ Benefits-Backend.Repository/Repositories/PensionRequestRepository.cs
@@ -0,0 +1,26 @@
+﻿using Benefits_Backend.Domain.Context;
+using Benefits_Backend.Domain.Entities;
+using Benefits_Backend.Repository.IRepositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Benefits_Backend.Repository.Repositories
+{
+    public class PensionRequestRepository : IPensionRequestRepository
+    {
+        private readonly ApplicationContext context;
+
+        public PensionRequestRepository(ApplicationContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<PensionRequest> Add(PensionRequest pensionRequest)
+        {
+           await context.PensionRequests.AddAsync(pensionRequest);
+            return pensionRequest;
+        }
+    }
+}
