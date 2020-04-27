1d9c724d67fa9331717017b411f0846deeac567b
Hagar <hagar.aboelfottouh1@vodafone.com>
2020-04-27 16:11:33 +0200

    add the service

---
+++ Benefits-Backend.Service/Services/PensionRequestService.cs
@@ -0,0 +1,24 @@
+﻿using Benefits_Backend.Domain.Entities;
+using Benefits_Backend.Repository.IRepositories;
+using Benefits_Backend.Service.IServices;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Benefits_Backend.Service.Services
+{
+    public class PensionRequestService : IPensionRequestService
+    {
+        private readonly IPensionRequestRepository pensionRequestRepository;
+
+        public PensionRequestService(IPensionRequestRepository pensionRequestRepository)
+        {
+            this.pensionRequestRepository = pensionRequestRepository;
+        }
+        public PensionRequest CreatePensionRequest(PensionRequest pensionRequest)
+        {
+            pensionRequestRepository.Add(pensionRequest);
+            return pensionRequest;
+        }
+    }
+}
