9cfca60527f516e9eae92b671774ced3cb61a246
Hagar <hagar.aboelfottouh1@vodafone.com>
2020-04-30 16:00:44 +0200

    add name and change numbers datatype

--- Benefits-Backend.Domain/Entities/MetlifeData.cs
+++ Benefits-Backend.Domain/Entities/MetlifeData.cs
@@ -7,18 +7,17 @@ namespace Benefits_Backend.Domain.Entities
    public class MetlifeData
     {
         public int Id { get; set; }
-
+        public string Name { get; set; }
         #region Metlife data
         public int CERT { get; set; }
-        public float OldBalance { get; set; }
-        public float Contribution { get; set; }
-        public float Income { get; set; }
-        public float Withdrawals { get; set; }
-        public float NewBalance { get; set; }
+        public double OldBalance { get; set; }
+        public double Contribution { get; set; }
+        public double Income { get; set; }
+        public double Withdrawals { get; set; }
+        public double NewBalance { get; set; }
         #endregion
 
         //public int StaffId { get; set; }
-        //public string Name { get; set; }
         //public string Band { get; set; }
         //public string CC { get; set; }
         //public float Tenure { get; set; }
