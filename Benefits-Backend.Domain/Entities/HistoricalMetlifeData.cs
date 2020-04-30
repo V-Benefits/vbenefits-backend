ddd60bf05a40c2825d7c7b34fd645abfacd3d43b
Hagar <hagar.aboelfottouh1@vodafone.com>
2020-04-30 16:01:23 +0200

    update historical metlife data table to remove the extra fields

--- Benefits-Backend.Domain/Entities/HistoricalMetlifeData.cs
+++ Benefits-Backend.Domain/Entities/HistoricalMetlifeData.cs
@@ -9,6 +9,7 @@ namespace Benefits_Backend.Domain.Entities
         public int UpdatedById { get; set; }
 
         #region Metlife data
+        public string Name { get; set; }
         public int CERT { get; set; }
         public float OldBalance { get; set; }
         public float Contribution { get; set; }
@@ -18,6 +19,8 @@ namespace Benefits_Backend.Domain.Entities
         #endregion
 
         #region Updated metlife data
+        public string UpdatedName { get; set; }
+
         public int UpdatedCERT { get; set; }
         public float UpdatedOldBalance { get; set; }
         public float UpdatedContribution { get; set; }
@@ -26,41 +29,41 @@ namespace Benefits_Backend.Domain.Entities
         public float UpdatedNewBalance { get; set; }
         #endregion
 
-        #region Old Data
-        public int StaffId { get; set; }
-        public string Name { get; set; }
-        public string Band { get; set; }
-        public string CC { get; set; }
-        public float Tenure { get; set; }
-        public string SubBand { get; set; }
-        public DateTime HiringDate { get; set; }
-        public float Vesting { get; set; }
-        public float SepRound { get; set; }
-        public bool EligibilityBasedOnLastRound { get; set; }
-        public float VestedBalance { get; set; }
-        public float MaxLoanAmount { get; set; }
-        public float ProrateContribution { get; set; }
-        public float BalanceAsOf28FebSimpleformula { get; set; }
+        //#region Old Data
+        //public int StaffId { get; set; }
+        //public string Name { get; set; }
+        //public string Band { get; set; }
+        //public string CC { get; set; }
+        //public float Tenure { get; set; }
+        //public string SubBand { get; set; }
+        //public DateTime HiringDate { get; set; }
+        //public float Vesting { get; set; }
+        //public float SepRound { get; set; }
+        //public bool EligibilityBasedOnLastRound { get; set; }
+        //public float VestedBalance { get; set; }
+        //public float MaxLoanAmount { get; set; }
+        //public float ProrateContribution { get; set; }
+        //public float BalanceAsOf28FebSimpleformula { get; set; }
 
-        #endregion
+        //#endregion
 
-        #region Updated data
+        //#region Updated data
 
-        public int UpdatedStaffId { get; set; }
-        public string UpdatedName { get; set; }
-        public string UpdatedBand { get; set; }
-        public string UpdatedCC { get; set; }
-        public float UpdatedTenure { get; set; }
-        public string UpdatedSubBand { get; set; }
-        public DateTime UpdatedHiringDate { get; set; }
-        public float UpdatedVesting { get; set; }
-        public float UpdatedSepRound { get; set; }
-        public bool UpdatedEligibilityBasedOnLastRound { get; set; }
-        public float UpdatedVestedBalance { get; set; }
-        public float UpdatedMaxLoanAmount { get; set; }
-        public float UpdatedProrateContribution { get; set; }
-        public float UpdatedBalanceAsOf28FebSimpleformula { get; set; }
-        #endregion
+        //public int UpdatedStaffId { get; set; }
+        //public string UpdatedName { get; set; }
+        //public string UpdatedBand { get; set; }
+        //public string UpdatedCC { get; set; }
+        //public float UpdatedTenure { get; set; }
+        //public string UpdatedSubBand { get; set; }
+        //public DateTime UpdatedHiringDate { get; set; }
+        //public float UpdatedVesting { get; set; }
+        //public float UpdatedSepRound { get; set; }
+        //public bool UpdatedEligibilityBasedOnLastRound { get; set; }
+        //public float UpdatedVestedBalance { get; set; }
+        //public float UpdatedMaxLoanAmount { get; set; }
+        //public float UpdatedProrateContribution { get; set; }
+        //public float UpdatedBalanceAsOf28FebSimpleformula { get; set; }
+        //#endregion
 
     }
 }
